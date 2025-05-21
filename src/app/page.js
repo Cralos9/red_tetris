c3934aaad609d771b2dd3d393e44602250bf4d42
Cralos9 <carlosc.rocha77@gmail.com>
2025-05-21 16:01:26 +0100

    starting tetris

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,36 @@
+'use client';
 import Image from "next/image";
 
 export default function Home() {
+  function Game()
+  {
+    console.log("Game")
+  }
+  function Login()
+  {
+    console.log("Game")
+  }
+  function SignOut()
+  {
+    console.log("Game")
+  }
   return (
-    <h1>
-      Teste
-    </h1>
+    <div>
+      <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet"></link>
+      <div className="header">
+          <h1 className="title"> Dr Tetris</h1>
+        <div className="logButton-cont">
+          <button onClick={Login} className="logButton">Log In</button>
+          <button onClick={SignOut} className="logButton">Sign Out</button>
+        </div>
+      </div>
+      <img className="image" src="/images/images.png" alt="Mario"></img>
+      <div className="button-container">
+          <button onClick={Game} className="button">Game</button>
+          <button className="button">Leaderboard</button>
+          <button className="button">Profile</button>
+      </div>
+    </div>
   );
 }
+
