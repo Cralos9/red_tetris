2034f9ed8b641b56657dd845ff243fd081047ac2
rmsa42 <rui.soares.almeida@outlook.pt>
2025-06-03 16:04:10 +0100

    Implemented Wall kicks

--- server/Piece.js
+++ server/Piece.js
@@ -1,11 +1,12 @@
 import { log } from "./debug.js"
 import { moveHorizontal, moveVertical, rotation } from "./movement.js"
 import { COLUMNS, ROWS } from "./gameParams.js"
-import { getRotations, getSkirt, compare } from "./utils.js"
+import { getRotations, getSkirt, compare, getKicks } from "./utils.js"
 
 export class Piece {
-	constructor(pieceCoor, color) {
+	constructor(pieceCoor, pieceOffsets, color) {
 		this.patterns = pieceCoor
+		this.offsets = pieceOffsets
 		this.skirts = [getSkirt(pieceCoor[0])]
 		for (let i = 0; i < 3; i++) {
 			const tmp = getRotations(this.patterns[i]).sort(compare)
@@ -89,10 +90,36 @@ export class Piece {
 		moveHorizontal(0)
 	}
 
-	rotate(r) {
-		this.index += r + 4
-		this.index = this.index % 4
-		log("Rotated Piece:", this.index)
+	checkRotation(field, kick, pattern) {
+		for (let k = 0; k < pattern.length; k++) {
+			const x = this.column + pattern[k][0] + kick[0]
+			const y = this.row + pattern[k][1] + kick[1]
+			log("WallKicks: (", y, ", ", x, ")")
+			if (x >= COLUMNS || x < 0 || y >= ROWS || y < 0 || field[y][x] > 0) {
+				log("Cant do this rotation")
+				return (false)
+			}
+		}
+		return (true)
+	}
+
+	rotate(field, r) {
+		let rot = this.index + r + 4
+		rot = rot % 4
+		const kicks = getKicks(this.offsets[this.index], this.offsets[rot])
+		const pattern = this.patterns[rot]
+		if (r === 1 || r === -1) {
+			for (let i = 0; i < kicks.length; i++) {
+				log("Kick:", kicks[i])
+				if (this.checkRotation(field, kicks[i], pattern)) {
+					this.row += kicks[i][1]
+					this.column += kicks[i][0]
+					this.index = rot
+					log("Rotated Piece:", this.index)
+					break
+				}
+			}
+		}
 		rotation(0)
 	}
 
--- server/tests/index.js
+++ server/tests/index.js
@@ -1,6 +1,6 @@
 import { Bag } from "../Bag.js"
-import { getKicks } from "../utils.js"
-import { commonOffsets } from "../gameParams.js"
+import { getKicks, getRotations } from "../utils.js"
+import { Icoor, JLTSZoffsets, Ioffsets, Tcoor } from "../gameParams.js"
 
 const bag = new Bag()
 
@@ -22,10 +22,13 @@ function bagTests() {
 }
 
 function offsets() {
-	const O = commonOffsets[0]
-	const R = commonOffsets[1]
-	const kicks = getKicks(O, R)
-	console.log("Kicks:", kicks)
+	const O = Ioffsets[0]
+	const R = Ioffsets[1]
+
+	const arr = getRotations(Icoor[0])
+	console.log("IRrot:", arr)
+//	const kicks = getKicks(O, R)
+//	console.log("Kicks:", kicks)
 }
 
 offsets()
