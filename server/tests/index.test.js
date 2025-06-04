795ce2931e46598e690f1ec4470fbfb0aeec7a90
rmsa42 <rui.soares.almeida@outlook.pt>
2025-06-04 15:37:33 +0100

    Starting making tests using Jest

---
+++ server/tests/index.test.js
@@ -0,0 +1,84 @@
+import { Bag } from "../Bag.js"
+import { getKicks, getRotations } from "../utils.js"
+import { Icoor, JLTSZoffsets, Ioffsets, Tcoor } from "../gameParams.js"
+import { Game } from "../game.js"
+import { COLUMNS, ROWS } from "../gameParams.js"
+
+const bag = new Bag()
+
+function printStack(stack) {
+	let out = ""
+	for (let i = 0; i < stack.length; i++) {
+		out += stack[i].toString() + " "
+	}
+	console.log(out)
+}
+
+function bagTests() {
+	printStack(bag.getStack())
+	console.log("Get Next Piece")
+	for (let i = 0; i < 8; i++) {
+		bag.getNextPiece()
+	}
+	printStack(bag.getStack())
+}
+
+function offsets() {
+	const O = Ioffsets[0]
+	const R = Ioffsets[1]
+
+	const arr = getRotations(Icoor[0])
+	console.log("IRrot:", arr)
+//	const kicks = getKicks(O, R)
+//	console.log("Kicks:", kicks)
+}
+
+function completeLines(nbr, field) {
+	for (let i = 0; i < nbr; i++) {
+		for (let k = 0; k < COLUMNS; k++) {
+			field[(ROWS - 1) - i][k] = 1
+		}
+	}
+}
+
+describe("PatternMatching (Mark Lines for Clear)", () => {
+	const game = new Game()
+	let nbrLines = 0
+	let data = game.hitList
+
+	beforeEach(() => {
+		completeLines(nbrLines, game.field)
+		game.patternMatch()
+		data = game.hitList
+	})
+
+	afterEach(() => {
+		game.hitList = []
+	})
+
+	test('Zero Lines Marked', () => {
+		expect(data.length).toEqual(nbrLines)
+	})
+	nbrLines = 1
+	test('One Line Marked', () => {
+		expect(data.length).toEqual(nbrLines)
+	})
+	nbrLines = 2
+	test('Two Line Marked', () => {
+		expect(data.length).toEqual(nbrLines)
+	})
+	nbrLines = 3
+	test('Two Line Marked', () => {
+		expect(data.length).toEqual(nbrLines)
+	})
+	nbrLines = 4
+	test('Two Line Marked', () => {
+		expect(data.length).toEqual(nbrLines)
+	})
+})
+
+test('Reset Coordinates in the matrix after collision', () => {
+	const piece = bag.getNextPiece()
+	const data = [piece.row, piece.column, piece.index]
+	expect(data).toEqual([1, 5, 0])
+})
