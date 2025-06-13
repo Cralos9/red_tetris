331aaec66e9257f9f54397ec5a7079de0d5f8975
Cralos9 <carlosc.rocha77@gmail.com>
2025-06-13 17:40:41 +0100

    Held and next piece

---
+++ src/app/[room]/[player_name]/functions.js
@@ -0,0 +1,140 @@
+
+function game(cells, field)
+{
+	for (let y = 0; y < 20; y++) 
+		{
+			for (let x = 0; x < 10; x++) 
+			{
+			  const index = y * 10 + x;
+			  const value = field[y][x];
+			  const cell = cells[index];
+		  
+				if (value !== 8)
+				{
+					cell.style.backgroundImage = 'none';
+					cell.style.backgroundColor = gameDraw.getColor(value);
+					cell.removeAttribute('data-virus');
+				} 
+				else 
+				{
+					if (!cell.hasAttribute('data-virus')) 
+					{
+					const randomGif = gifs[Math.floor(Math.random() * gifs.length)];
+					cell.setAttribute('data-virus', randomGif);
+					}
+					const virusGif = cell.getAttribute('data-virus');
+					cell.style.backgroundImage = `url('${virusGif}')`;
+					cell.style.backgroundSize = "cover";
+				}
+			}
+		}
+}
+
+function add_secondary_cells(div,  amount)
+{
+	for (let i = 0; i < amount; i++) 
+	{
+		const cell = document.createElement('div');
+		if (amount === 200)
+			cell.className = 'cell';
+		else
+		cell.className = 'cell2';
+		div.appendChild(cell);
+	}
+}
+
+	function add_cells(string,  amount)
+	{
+		const div = document.querySelector(string);
+		div.innerHTML = '';
+		for (let i = 0; i < amount; i++) 
+		{
+			const cell = document.createElement('div');
+			if (amount === 200)
+				cell.className = 'cell';
+			else
+			cell.className = 'cell2';
+			div.appendChild(cell);
+		}
+	}
+
+function getColor(value)
+{
+	const colors = {
+	"-1": '#ffffffCC',
+	0: 'transparent',
+	1: '#b2ffff',
+	2: '#d6a4ff',
+	3: '#a3c4ff',
+	4: '#ffd1a4',
+	5: '#ffffb2',
+	6: '#b2ffb2',       
+	7: '#ffb2b2'
+	};
+	
+	return colors[value];
+}
+
+function heldPieceDraw(heldPiece)
+{
+	if (heldPiece != 0)
+	{
+		const held = document.querySelector('.held-piece');
+		const cells = held.querySelectorAll('.cell2');
+		cells.forEach((cell) =>
+		{
+			cell.style.backgroundColor = getColor(0);
+			cell.style.border = '0px solid #222';
+		});
+		const centerX = 2;
+		const centerY = 2;
+		heldPiece.hold.forEach(([x, y]) => {
+			const drawX = centerX + x;
+			const drawY = centerY + y;
+			const index = drawY * 6 + drawX;
+			const cell = cells[index];
+			cell.style.backgroundColor = getColor(heldPiece.color);
+			cell.style.border = '2px solid #222';
+		});
+	}
+}
+
+function nextPieceDraw(nextPiece)
+{
+	const next = document.querySelector('.next-piece');
+	const centerX = 2;
+	let centerY = 2;
+	const cells = next.querySelectorAll('.cell2');
+	cells.forEach((cell) =>
+	{
+		cell.style.backgroundColor = getColor(0);
+		cell.style.border = '0px solid #222';
+	});
+	nextPiece.forEach((piece) => {
+		if (centerY > 8)
+			return;
+		piece.piece.forEach(([x, y]) => {
+			const drawX = centerX + x;
+			const drawY = centerY + y;
+			const index = drawY * 6 + drawX;
+			const cell = cells[index];
+			if (cell)
+			{
+				cell.style.backgroundColor = getColor(piece.color);
+				cell.style.border = '2px solid #222';
+			}
+		});
+		centerY += 3;
+	});
+}
+
+const gameDraw = {
+	heldPieceDraw,
+	nextPieceDraw,
+	getColor,
+	add_cells,
+	add_secondary_cells,
+	game
+};
+
+export default gameDraw;
--- src/app/[room]/[player_name]/page.js
+++ src/app/[room]/[player_name]/page.js
@@ -2,8 +2,14 @@
 import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { socket } from "../../../socket";
+import  gameDraw  from "./functions";
 
 export default function RoomPage() {
+	const gifs = [
+		"/images/blue_virus.gif",
+		"/images/yellow_virus.gif",
+		"/images/red_virus.gif"
+	  ];
 	const params = useParams();
 	const roomCode = params.room;
 	const [gameOver, setGameOver] = useState(false);
@@ -17,66 +23,6 @@ export default function RoomPage() {
 	function end_game() {
 	  setIsDisabled(false);
 	  setGameOver(true);
-	}
-
-	function nextPieceDraw(nextPiece)
-	{
-		if(nextPiece != 0)
-		{
-			const next = document.querySelector('.next-piece');
-			const centerX = 2;
-			var centerY = 2;
-			const cells = next.querySelectorAll('.cell2');
-			cells.forEach((cell) =>
-			{
-				cell.style.backgroundColor = getColor(0);
-				cell.style.border = '0px solid #222';
-			});
-			nextPiece.forEach((piece) => {
-				if (centerY > 8)
-					return
-				piece.forEach(([x, y]) => {
-					const drawX = centerX + x;
-					const drawY = centerY + y;
-					const index = drawY * 6 + drawX;
-					const cell = cells[index];
-					if (cell)
-					{
-						cell.style.backgroundColor = getColor(1);
-						cell.style.border = '2px solid #222';
-					}
-				});
-				centerY += 3;
-			});
-		}
-	}
-
-	function add_cells(div, amount)
-	{
-		for (let i = 0; i < amount; i++) 
-		{
-			const cell = document.createElement('div');
-			if (amount === 200)
-				cell.className = 'cell';
-			else
-			cell.className = 'cell2';
-			div.appendChild(cell);
-		}
-	}
-	function getColor(value)
-	{
-		const colors = {
-			0: 'transparent',
-			1: '#b2ffff',
-			2: '#d6a4ff',
-			3: '#a3c4ff',
-			4: '#ffd1a4',
-			5: '#ffffb2',
-			6: '#b2ffb2',       
-			7: '#ffb2b2',
-		};
-		
-		return colors[value];
 	}
 
 	useEffect(() => {
@@ -109,99 +55,43 @@ export default function RoomPage() {
 					otherBoard = document.createElement('div');
 					otherBoard.className = 'secondary-game';
 					otherBoard.id = otherBoards[i];
-					add_cells(otherBoard, 200);
+					gameDraw.add_secondary_cells(otherBoard, 200);
 					document.querySelector('.secondary-games').appendChild(otherBoard);
 				}
 			}
 		})
 		socket.on('game', (msg) => {
-			const field = msg.field;
-			if (!msg.running) {
+			if (!msg.running && msg.playerId === socket.id) {
 				end_game();
 				return;
 			}
-		
+			const field = msg.field;
+			var cells 
 			if (msg.playerId === socket.id) {
-				const cells = document.querySelectorAll('.game-bottle .cell');
+				cells = document.querySelectorAll('.game-bottle .cell');
 				const heldPiece = msg.holdPiece
 				const nextPiece = msg.nextPiece
-				nextPieceDraw(nextPiece);
-				if (heldPiece != 0)
-				{
-					const cells = held.querySelectorAll('.cell2');
-					cells.forEach((cell) =>
-					{
-						cell.style.backgroundColor = getColor(0);
-						cell.style.border = '0px solid #222';
-					});
-					const centerX = 2;
-					const centerY = 2;
-					heldPiece.forEach(([x, y]) => {
-						const drawX = centerX + x;
-						const drawY = centerY + y;
-						const index = drawY * 6 + drawX;
-						const cell = cells[index];
-						cell.style.backgroundColor = getColor(1);
-						cell.style.border = '2px solid #222';
-					});
-				}
-				for (let y = 0; y < 20; y++) {
-					for (let x = 0; x < 10; x++) 
-					{
-						const index = y * 10 + x;
-						const value = field[y][x];
-						if (value !== 8) 
-						{
-							cells[index].style.backgroundImage = 'none';
-							cells[index].style.backgroundColor = getColor(value);
-						} 
-						else 
-						{
-							cells[index].style.backgroundImage = "url('/images/blue_virus.gif')";
-							cells[index].style.backgroundSize = "cover";
-						}
-					}
-				}
+				gameDraw.nextPieceDraw(nextPiece);
+				gameDraw.heldPieceDraw(heldPiece);
+				const randomGif = gifs[Math.floor(Math.random() * gifs.length)];
 			} 
 			else 
 			{
 				let otherBoard = document.getElementById(msg.playerId);
-				const gameCells = otherBoard.querySelectorAll('.cell');
-				for (let y = 0; y < 20; y++) {
-					for (let x = 0; x < 10; x++) {
-						const index = y * 10 + x;
-						const value = field[y][x];
-						if (gameCells[index]) {
-							if (value !== 8) {
-								gameCells[index].style.backgroundImage = 'none';
-								gameCells[index].style.backgroundColor = getColor(value);
-							} else {
-								gameCells[index].style.backgroundImage = "url('/images/blue_virus.gif')";
-								gameCells[index].style.backgroundSize = "cover";
-							}
-						}
-					}
-				}
+				cells = otherBoard.querySelectorAll('.cell');
 			}
-		});		
+			gameDraw.game(cells, field)
+		});
+
 		document.addEventListener("keydown", e => {
 			socket.emit("keyDown", {key: e.key, roomCode: roomCode})
 		})
-		const bottle = document.querySelector('.game-bottle');
-		bottle.innerHTML = '';
-		add_cells(bottle, 200)
-
+		
+		gameDraw.add_cells('.game-bottle', 200)
 		const game22 = document.querySelector('.secondary-games');
 		game22.innerHTML = '';
-
-		const next = document.querySelector('.next-piece');
-		next.querySelectorAll('.cell').forEach(cell => cell.remove());		
-		add_cells(next, 60)
-
-		const held = document.querySelector('.held-piece');
-		held.querySelectorAll('.cell').forEach(cell => cell.remove());
-		add_cells(held, 30)
-
+		gameDraw.add_cells('.next-piece', 60)
+		gameDraw.add_cells('.held-piece', 30)
 		if (name) 
 			setUsername(name);
 	}, [name]);
@@ -249,7 +139,6 @@ export default function RoomPage() {
 					i = -1;
 				}
 			}
-	
 			top5.forEach((entry, index) => {
 				localStorage.setItem(`Score${index + 1}`, `${entry.name} ${entry.score}`);
 			});
