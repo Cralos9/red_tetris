acfd3cd64d2383ece4bf4c96d23fdd202025e453
Cralos9 <carlosc.rocha77@gmail.com>
2025-07-07 14:54:11 +0100

    ARR and DAS Slider

--- src/app/options/page.js
+++ src/app/options/page.js
@@ -1,51 +1,65 @@
 'use client';
+import { useRouter } from 'next/navigation';
 import { useState } from "react";
+import { useEffect} from 'react';
 
 export default function Options() {
-  const [value, setValue] = useState(10);
-  const [arrValue, setarrValue] = useState(5);
+	const [value, setValue] = useState(10);
+	const [arrValue, setArrValue] = useState(5);
+	
+	useEffect(() => {
+		const savedDAS = localStorage.getItem("DAS");
+		if (savedDAS !== null) 
+			setValue(Number(savedDAS));
+
+		const savedARR = localStorage.getItem("ARR");
+		if (savedARR !== null) 
+			setArrValue(Number(savedARR));
+	}, []);
+  const router = useRouter();
 
   const maxDAS = 20;
   const maxARR = 10;
 
   function apply_options()
   {
-    localStorage.setItem("DAS", value)
-    localStorage.setItem("ARR", arrValue)
+	localStorage.setItem("DAS", value);
+	localStorage.setItem("ARR", arrValue);
+	router.push('/game');
   }
 
 
   return (
-    <div className="main-container">
-      <div className="usercard">
-        <button>
-          <span>DAS</span>
-          <input
-            type="range"
-            min="0"
-            max={maxDAS}
-            value={maxDAS - value}
-            onChange={(e) => setValue(maxDAS - Number(e.target.value))}
-          />
-          <p>{value} Frames</p>
-        </button>
+	<div className="main-container">
+	  <div className="usercard">
+		<button>
+		  <span>DAS</span>
+		  <input
+			type="range"
+			min="0"
+			max={maxDAS}
+			value={maxDAS - value}
+			onChange={(e) => setValue(maxDAS - Number(e.target.value))}
+		  />
+		  <p>{value} Frames</p>
+		</button>
 
-        <button>
-          <span>ARR</span>
-          <input
-            type="range"
-            min="0"
-            max={maxARR}
-            value={maxARR - arrValue}
-            onChange={(e) => setarrValue(maxARR - Number(e.target.value))}
-          />
-          <p>{arrValue} Frames</p>
-        </button>
-        <div className="button-row">
-        <button className="button2" onClick={apply_options}>Apply</button>
-        <button className="button2">Reset</button>
-        </div>
-      </div>
-    </div>
+		<button>
+		  <span>ARR</span>
+		  <input
+			type="range"
+			min="0"
+			max={maxARR}
+			value={maxARR - arrValue}
+			onChange={(e) => setArrValue(maxARR - Number(e.target.value))}
+		  />
+		  <p>{arrValue} Frames</p>
+		</button>
+		<div className="button-row">
+		<button className="button2" onClick={apply_options}>Apply</button>
+		<button className="button2">Reset</button>
+		</div>
+	  </div>
+	</div>
   );
 }
