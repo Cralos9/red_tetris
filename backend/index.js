0e60bf068d992da6aafcd141dcc2977f515fe771
rmsa42 <rui.soares.almeida@outlook.pt>
2025-09-17 14:47:15 +0100

    Fixed hardcoded route

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 import { createServer } from "node:http"
+import path from "node:path"
 import express from "express"
 import { Server } from "socket.io"
 import { playerHandlers, gameHandlers } from "./handlers.js"
 
 const hostname = process.env.HOST
 const port = process.env.PORT
+const staticFilesDir = process.env.STATIC_FILES_DIR
 
 const RoomsMap = new Map()
 
@@ -12,13 +14,18 @@ const app = express()
 const server = createServer(app)
 const io = new Server(server, { 'pingInterval': 7000, 'pingTimeout': 8000 })
 
-app.use(express.static("dist"))
+app.use(express.static(staticFilesDir))
 
-// Small fix for this to work (need to think more about it)
-app.get('/:room/:player_name', (req, res) => {
-	console.log(`Url request to ${req.params.room}/${req.params.player_name}`)
-	res.sendFile("/home/rumachad/red_tetris/dist/index.html")
+// Catches all get requests
+app.get(/.*/, (req, res) => {
+	res.sendFile(path.join(import.meta.dirname, "../", staticFilesDir, "index.html"))
 })
+
+// Only for joining games via url
+//app.get('/:room/:player_name', (req, res) => {
+//	console.log(`Url request to ${req.params.room}/${req.params.player_name}`)
+//	res.sendFile(path.join(import.meta.dirname, "../", staticFilesDir, "index.html"))
+//})
 
 io.on('connection', (socket) => {
 	console.log("New Connection:", socket.id)
